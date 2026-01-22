57db27459d3ec50a52fb4a2a5c9b2daa18c4e9d5
Juan García <juan.garciadieguez@telefonica.com>
2026-01-22 17:40:01 +0100

    add button component with different themes

---
+++ components/Button.tsx
@@ -0,0 +1,68 @@
+import { FontAwesome } from "@expo/vector-icons";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+type Props = {
+    label: string,
+    theme?: 'primary',
+};
+
+export default function Button({ label, theme }: Props) {
+    if (theme === 'primary') {
+        return (
+            <View style={[styles.buttonContainer,
+            { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 }
+            ]}>
+                <Pressable
+                    style={[styles.button, { backgroundColor: '#fff' }]}
+                    onPress={() => { alert("You pressed a button!") }}
+                >
+                    <FontAwesome
+                        name="picture-o"
+                        size={18}
+                        color="#25292e"
+                        style={styles.buttonIcon}
+                    />
+                    <Text style={[styles.buttonText, { color: '#25292e' }]}>{label}</Text>
+                </Pressable>
+
+            </View>
+        );
+    } else {
+        return (<View style={styles.buttonContainer}>
+            <Pressable
+                style={styles.button}
+                onPress={() => { alert("You pressed a button!") }}
+            >
+                <Text style={styles.buttonText}>{label}</Text>
+            </Pressable>
+
+        </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        width: 320,
+        height: 68,
+        marginHorizontal: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 4,
+    },
+    button: {
+        borderRadius: 8,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    buttonIcon: {
+        paddingRight: 8,
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+    },
+});
